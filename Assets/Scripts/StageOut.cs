dee34da264843c408c8f020de10bcbee9e66a3eb
Sho-0124 <sankado657@gmail.com>
2026-06-18 18:45:53 +0900

    TMPを追加
    TMP!!

--- Assets/Scripts/StageOut.cs
+++ Assets/Scripts/StageOut.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class StageOut : MonoBehaviour
 {
     private int Score;
+    public TextMeshProUGUI scoreText;
     void OnTriggerEnter(Collider other)
     {
         Score = Score + 1;
-        Debug.Log($"スコア：{Score}");
+        scoreText.text = $"スコア：{Score}";
         //Debug.Log($"{other.name}がすり抜けた");
         Destroy(other.gameObject);
     }
