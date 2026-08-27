a511743c8588f51149a2904f11bf5f10ee7f8e1b
Sho-0124 <sankado657@gmail.com>
2026-08-27 18:48:41 +0900

    綺麗なUI追加

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -3,54 +3,68 @@ using TMPro;
 
 public class ItemManager : MonoBehaviour
 {
+    // アイテムの所持数
     public int grapeCount = 0;
     public int orangeCount = 0;
     public int melonCount = 0;
     public int colaCount = 0;
     public int sodaCount = 0;
 
+    // 合計スコア
+    public int totalScore = 0;
+
+    // UI
     public TMP_Text itemCountText;
+    public TMP_Text scoreText;
 
     private void Start()
     {
         UpdateUI();
     }
 
+    // アイテムを取得したときに呼ばれる
     public void GetItem(string itemName)
     {
         if (itemName == "ぶどう味")
         {
             grapeCount++;
+            totalScore += 1;
         }
         else if (itemName == "みかん味")
         {
             orangeCount++;
+            totalScore += 2;
         }
-        else if (itemName == "メロン味")
+        else if (itemName == "キウイ味")
         {
             melonCount++;
+            totalScore += 3;
         }
         else if (itemName == "コーラ味")
         {
             colaCount++;
+            totalScore += 4;
         }
         else if (itemName == "ソーダ味")
         {
             sodaCount++;
+            totalScore += 5;
         }
-
-        Debug.Log(itemName + "をGET！");
 
         UpdateUI();
     }
 
+    // 画面表示を更新
     private void UpdateUI()
     {
         itemCountText.text =
             "ぶどう味：" + grapeCount + "\n" +
             "みかん味：" + orangeCount + "\n" +
-            "メロン味：" + melonCount + "\n" +
+            "キウイ味：" + melonCount + "\n" +
             "コーラ味：" + colaCount + "\n" +
             "ソーダ味：" + sodaCount;
+
+        // スコアを「スコア：0」の形式で表示
+        scoreText.text = "スコア：" + totalScore;
     }
 }
